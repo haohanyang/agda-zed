ffe74df7cd9665776ecd27ed2ab55e8bcfad9e85
Haohan Yang <yanghaohanmail@gmail.com>
2026-03-12 23:13:49 +0100

    Add function and data type to outline

--- languages/agda/outline.scm
+++ languages/agda/outline.scm
@@ -3,5 +3,33 @@
     (function_name
       (atom
         (qid) @name)))
-    (rhs (expr) @context)
+  (rhs (expr) @context)
+) @item
+
+(data_signature
+  (data_name) @name
+  (expr) @context
+) @item
+
+(data
+  (data_name) @name
+  (expr) @context
+) @item
+
+(data
+  (data_name) @name
+) @item
+
+(record_signature
+  (record_name) @name
+  (expr) @context
+) @item
+
+(record
+  (record_name) @name
+  (expr) @context
+) @item
+
+(record
+  (record_name) @name
 ) @item
